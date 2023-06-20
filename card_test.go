0cfcccf6f4a2852c4b9d0d7b6dda1c6c231b3b8b
Paul Santiago <paulsantiago282@gmail.com>
2023-06-20 15:57:42 -0400

    modify test

--- card_test.go
+++ card_test.go
@@ -4,7 +4,16 @@ import "fmt"
 
 func ExampleCard() {
 	fmt.Println(Card{Rank: Ace, Suit: Heart})
+	fmt.Println(Card{Rank: Two, Suit: Spade})
+	fmt.Println(Card{Rank: Three, Suit: Diamond})
+	fmt.Println(Card{Rank: Four, Suit: Club})
+	fmt.Println(Card{Suit: Joker})
 
 	// Output:
 	// Ace of Hearts
+	// Two of Spades
+	// Three of Diamonds
+	// Four of Clubs
+	// Joker
+
 }
