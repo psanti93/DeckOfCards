289857fc7b32eaf9d9d2b880b137415d6738b29b
Paul Santiago <paulsantiago282@gmail.com>
2023-06-20 18:37:31 -0400

    Add optional functions to sort cards by default or customly

--- cards.go
+++ cards.go
@@ -1,7 +1,10 @@
 //go:generate stringer -type=Suit,Rank
 package deck
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type Suit uint8
 
@@ -52,18 +55,43 @@ func (c Card) String() string {
 	return fmt.Sprintf("%s of %ss", c.Rank, c.Suit)
 }
 
-func New() []Card {
+func New(opts ...func([]Card) []Card) []Card {
 	var cards []Card
-
-	// TODO
-	// for each suit
 	for _, suit := range suits {
-		// for each rank
 		for rank := minRank; rank <= maxRank; rank++ {
-			// add card(suit,rank) to cards
 			cards = append(cards, Card{Suit: suit, Rank: rank})
 		}
 	}
 
+	for _, opt := range opts {
+		cards = opt(cards)
+	}
+
 	return cards
+}
+
+// sort.Slice(any, less func(i,j int) bool)
+func DefaultSort(cards []Card) []Card {
+	sort.Slice(cards, Less(cards))
+	return cards
+}
+
+// This one allows you to take in a custom less function that has a slice of cards and returns a boolean
+func Sort(less func(cards []Card) func(i, j int) bool) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		sort.Slice(cards, less(cards))
+		return cards
+	}
+}
+
+// I'm taking the first index and comparing it to the second index and if the first one is less than the second one return true
+func Less(cards []Card) func(i, j int) bool {
+	return func(i, j int) bool {
+		return absRank(cards[i]) < absRank(cards[j])
+	}
+}
+
+func absRank(c Card) int {
+
+	return int(c.Suit)*int(maxRank) + int(c.Rank)
 }
